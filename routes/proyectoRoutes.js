c5ebfb62c587e3b1532fc1e67a98e132a98141f1
Alejandro <ale.nico230398@gmail.com>
2024-11-21 20:30:20 -0300

    validar admin llamar a todos los proyectos

--- routes/proyectoRoutes.js
+++ routes/proyectoRoutes.js
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import ProyectoControllers from "../controllers/ProyectoControllers.js";
+import { validarAdmin } from "../middleware/validarAdmin.js";
 
 const proyectoController = new ProyectoControllers();
 const proyectoRoutes = Router();
 
-proyectoRoutes.get("/",proyectoController.getAllProyectos);
 proyectoRoutes.get("/:id",proyectoController.getProyectoById);
 proyectoRoutes.get("/user/:userId",proyectoController.getProyectosByUser);
 proyectoRoutes.post("/",proyectoController.createProyecto);
 proyectoRoutes.put("/:id",proyectoController.updateProyecto);
 proyectoRoutes.delete("/:id",proyectoController.deleteProyecto);
+
+proyectoRoutes.use(validarAdmin);
+proyectoRoutes.get("/",proyectoController.getAllProyectos);
 
 export default proyectoRoutes;
